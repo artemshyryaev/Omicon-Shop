3f420ed92602abca927d1e2f6032d1a12b8e69ed
Artem Shyriaiev <a.shyriaiev@sana-commerce.com>
2019-01-31 16:34:52 +0200

    Implement shippingAddress model

---
+++ Startersite/Startersite/Controllers/CheckoutController.cs
@@ -0,0 +1,17 @@
+﻿using Startersite.Models.ModelViews;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Startersite.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        public ActionResult ShippingInformationStep()
+        {
+            return View(new ShippingInformationModel());
+        }
+    }
+}
---
+++ Startersite/Startersite/Models/ModelViews/CheckoutModelView/ShippingInformationModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Startersite.Models.ModelViews
+{
+    public class ShippingInformationModel
+    {
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "Enter name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Surname")]
+        [Required(ErrorMessage = "Enter surname")]
+        public string Surname { get; set; }
+
+        [Display(Name = "E-mail")]
+        [Required(ErrorMessage = "Enter valid E-mail")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Display(Name="Phone numbet")]
+        [Required(ErrorMessage ="Enter valid telephone number")]
+        [DataType(DataType.PhoneNumber)]
+        public double PhoneNumber { get; set; }
+
+        [Display(Name = "Country")]
+        [Required(ErrorMessage = "Enter country")]
+        public string Country { get; set; }
+
+        [Display(Name = "Address")]
+        [Required(ErrorMessage = "Enter address")]
+        public string Address { get; set; }
+
+        [Display(Name = "Address")]
+        public string Address2 { get; set; }
+
+        [Display(Name = "City")]
+        [Required(ErrorMessage = "Enter city")]
+        public string City { get; set; }
+
+        [Display(Name = "Zip code")]
+        [Required(ErrorMessage = "Enter valid zip code")]
+        [DataType(DataType.PostalCode)]
+        public string ZipCode { get; set; }
+    }
+}
