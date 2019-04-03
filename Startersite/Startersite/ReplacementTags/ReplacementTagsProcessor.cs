cb272dc718b8d06abf328da7b4127cef07a1510a
Artem Shyriaiev <a.shyriaiev@sana-commerce.com>
2019-04-03 16:17:02 +0300

    Start creatin replacement tags and mail using .resx file

---
+++ Startersite/Startersite/ReplacementTags/ReplacementTagsProcessor.cs
@@ -0,0 +1,39 @@
+﻿using Startersite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace Startersite.ReplacementTags
+{
+    public class OrderReplacementTagsProcessor : IReplacementTagsProcessor
+    {
+         Dictionary<string, TagReplacer> tagReplacers;
+        StringBuilder text;
+        Order order;
+
+        Regex regex = new Regex();
+
+        public OrderReplacementTagsProcessor(StringBuilder text, Order order)
+        {
+            this.text = text;
+            this.order = order;
+        }
+
+        public string Process()
+        {
+            return regex.Replace(text, match =>
+           {
+               return 
+           }
+            );
+        }
+
+        public string ReplaceTags(Match m)
+        {
+
+        }
+    }
+}
---
+++ Startersite/Startersite/ReplacementTags/TagReplacer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Startersite.ReplacementTags
+{
+    public abstract class TagReplacer
+    {
+        public abstract string Tag { get; }
+    }
+}
