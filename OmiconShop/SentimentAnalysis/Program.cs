5e5b5c320f7291e2d664598c66f3975e8102dbf1
Artem Shyriaiev <a.shyriaiev@sana-commerce.com>
2019-06-06 16:49:11 +0300

    start with ml.net loadDatam buildAndTrainModel, Evaluate

---
+++ OmiconShop/SentimentAnalysis/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using static Microsoft.ML.DataOperationsCatalog;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms.Text;
+
+namespace SentimentAnalysis
+{
+    class Program
+    {
+        static readonly string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "yelp_labelled.txt");
+        static readonly string _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "Model.zip");
+
+        static void Main(string[] args)
+        {
+            MLContext mlContext = new MLContext();
+            var splitDataView = LoadData(mlContext);
+            var model = BuildAndTrainModel(mlContext, splitDataView.TrainSet);
+            Evaluate(mlContext, model, splitDataView.TestSet);
+        }
+
+        private static TrainTestData LoadData(MLContext mlContext)
+        {
+            var dataView = mlContext.Data.LoadFromTextFile<SentimentData>(_dataPath, hasHeader: false);
+
+            return mlContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
+        }
+
+        private static ITransformer BuildAndTrainModel(MLContext mlContext, IDataView splitTrainSet)
+        {
+            var estimator = mlContext.Transforms.Text.FeaturizeText(outputColumnName: "Features", inputColumnName: nameof(SentimentData.SentimentText))
+            .Append(mlContext.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: "Label", featureColumnName: "Features"));
+
+            return estimator.Fit(splitTrainSet);
+        }
+
+        private static void Evaluate(MLContext mlContext, ITransformer model, IDataView splitTestSet)
+        {
+            var predictions = model.Transform(splitTestSet);
+            var metrics = mlContext.BinaryClassification.Evaluate(predictions, "Label");
+        }
+    }
+}
