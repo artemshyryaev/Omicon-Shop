fa9effdf1967f97f52d3360628bfe7f6b4878c2b
Artem Shyriaiev <a.shyriaiev@sana-commerce.com>
2019-06-13 13:02:41 +0300

    Add class active to links in admin nav menu

---
+++ OmiconShop/OmiconShop.WebUI/HtmlHelpers/ElementAttributesHelpers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace OmiconShop.WebUI.HtmlHelpers
+{
+    public static class ElementAttributesHelpers
+    {
+        public static string IsActive<TModel>(this HtmlHelper<TModel> htmlHelper, string controller, string action)
+        {
+            var routeData = htmlHelper.ViewContext.RouteData;
+
+            var routeController = routeData.Values["controller"].ToString();
+            var routeAction = routeData.Values["action"].ToString();
+
+            return (controller == routeController && action == routeAction) ? "nav-link active" : "nav-link";
+        }
+    }
+}
